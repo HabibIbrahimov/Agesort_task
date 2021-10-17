5666a87cb1b2b5fe3489d193444613505e0b70a9
HabibIbrahimov <habibyi@code.edu.az>
2021-10-17 23:37:44 +0400

    added projectt

---
+++ Agesort/Persons.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Agesort
+{
+    class Persons: IComparable
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public Persons(string name,string surname,int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+
+        public int CompareTo(object obj)
+        {
+            int result = 1;
+            if (obj != null && obj is Persons)
+            {
+                Persons person = (Persons)obj;
+                result = this.Age.CompareTo(person.Age);
+            }
+            return result;
+        }
+        public override string ToString()
+        {
+            return String.Format("{0} - {1}", this.Name, this.Age);
+        }
+    }
+}
---
+++ Agesort/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Agesort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persons p1 = new Persons("Habib","Ibrahimov",25);
+            Persons p2 = new Persons("Rasul","Abbasov",13);
+            Persons p3 = new Persons("Hesen","Hesenli",23);
+            Persons p4 = new Persons("Ulvi","Memmedov",18);
+            List<Persons> list = new List<Persons>();
+            list.Add(p1);
+            list.Add(p2);
+            list.Add(p3);
+            list.Add(p4);
+            list.Sort();
+            
+            foreach (Persons p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+    }
+}
